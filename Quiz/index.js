d21f249b40aac3262efb74500a6c69c57300149e
LauM727 <103261360+LauM727@users.noreply.github.com>
2023-02-07 08:09:29 -0500

    Add files via upload

---
+++ Quiz/index.js
@@ -0,0 +1,17 @@
+
+let i=10;
+
+
+if(i%3===0){
+    console.log("Es divisible por 3");
+}else if(i%3==!0){
+    console.log("No es divisible por 3");
+
+}else if(i%5===0){
+    console.log("Es divisible por 5");
+}else if(i%5==!0){
+    console.log("No es divisible por 5");
+
+}else{
+    console.log("No es diviible ni por 5 y 3")
+}
